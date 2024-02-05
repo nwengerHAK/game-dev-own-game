475e0f1fc9beabf1fd03c23d7c1968e48181a4d7
Nico Wenger <nwenger@schueler.hakzell.at>
2024-02-05 11:15:21 +0100

    import backgrounds and bosses

--- src/index.js
+++ src/index.js
@@ -1,44 +1,62 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import bg1 from './assets/backgrounds/Cartoon_Forest_BG_01/Cartoon_Forest_BG_01.png'
+import bg2 from './assets/backgrounds/Cartoon_Forest_BG_02/Cartoon_Forest_BG_02.png'
+import bg3 from './assets/backgrounds/Cartoon_Forest_BG_03/Cartoon_Forest_BG_03.png'
+import bg4 from './assets/backgrounds/Cartoon_Forest_BG_04/Cartoon_Forest_BG_04.png'
+
+import boss1Idle from './assets/bosses/1/Idle.png'
+import boss1attack1 from './assets/bosses/1/Attack1.png'
+import boss1attack2 from './assets/bosses/1/Attack2.png'
+import boss1attack3 from './assets/bosses/1/Attack3.png'
+import boss1attack4 from './assets/bosses/1/Attack4.png'
+import boss1walk from './assets/bosses/1/Walk.png'
+import boss1death from './assets/bosses/1/Death.png'
+import boss1hurt from './assets/bosses/1/Hurt.png'
+import boss1special from './assets/bosses/1/Special.png'
+
+import boss2Idle from './assets/bosses/2/Idle.png'
+import boss2attack1 from './assets/bosses/2/Attack1.png'
+import boss2attack2 from './assets/bosses/2/Attack2.png'
+import boss2attack3 from './assets/bosses/2/Attack3.png'
+import boss2attack4 from './assets/bosses/2/Attack4.png'
+import boss2walk from './assets/bosses/2/Walk.png'
+import boss2death from './assets/bosses/2/Death.png'
+import boss2hurt from './assets/bosses/2/Hurt.png'
+import boss2special from './assets/bosses/2/Special.png'
+import boss2proj from './assets/bosses/2/Projectile.png'
+
+import boss3Idle from './assets/bosses/3/Idle.png'
+import boss3attack1 from './assets/bosses/3/Attack1.png'
+import boss3attack2 from './assets/bosses/3/Attack2.png'
+import boss3attack3 from './assets/bosses/3/Attack3.png'
+import boss3attack4 from './assets/bosses/3/Attack4.png'
+import boss3walk from './assets/bosses/3/Walk.png'
+import boss3death from './assets/bosses/3/Death.png'
+import boss3hurt from './assets/bosses/3/Hurt.png'
+import boss3special from './assets/bosses/3/Special.png'
+import boss3proj from './assets/bosses/3/Projectile.png'
+
+
+
 
 class MyGame extends Phaser.Scene
 {
-    constructor ()
-    {
-        super();
-    }
 
     preload ()
     {
-        //  This is an example of a bundled image:
-        this.load.image('logo', logoImg);
 
-        //  This is an example of loading a static image from the public folder:
-        this.load.image('background', 'assets/bg.jpg');
     }
       
     create ()
     {
-        this.add.image(400, 300, 'background');
 
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     scene: MyGame
 };
 
